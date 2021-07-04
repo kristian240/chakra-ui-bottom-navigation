9577b034a8f8f6cc906ac87450a5efdf9f6b63da
kristian240 <kristian.djakovic@infinum.hr>
2021-07-04 12:36:15 +0200

    Create bottom navigation

--- src/bottom-navigation.tsx
+++ src/bottom-navigation.tsx
@@ -1,1 +1,166 @@
-export const BottomNavigation = 0;
+import {
+	As,
+	chakra,
+	forwardRef,
+	HTMLChakraProps,
+	Icon,
+	IconProps,
+	omitThemingProps,
+	StylesProvider,
+	SystemStyleObject,
+	ThemingProps,
+	useMultiStyleConfig,
+	useStyles,
+} from '@chakra-ui/react';
+import { dataAttr, cx, __DEV__ } from '@chakra-ui/utils';
+import { mergeRefs } from '@chakra-ui/react-utils';
+import React from 'react';
+
+import {
+	BottomNavigationDescendantsProvider,
+	BottomNavigationItemProvider,
+	BottomNavigationProvider,
+	IBottomNavigationContext,
+	useBottomNavigationContext,
+	useBottomNavigationDescendant,
+	useBottomNavigationDescendants,
+	useBottomNavigationItemContext,
+} from './use-bottom-navigation';
+
+export interface IBottomNavigationProps
+	extends Omit<HTMLChakraProps<'nav'>, 'onChange'>,
+		ThemingProps<'BottomNavigation'>,
+		IBottomNavigationContext {}
+
+export const BottomNavigation = forwardRef<IBottomNavigationProps, 'nav'>(
+	({ value, onChange, showLabel, ...props }, ref) => {
+		const styles = useMultiStyleConfig('BottomNavigation', props);
+		const ownProps = omitThemingProps(props);
+
+		const descendants = useBottomNavigationDescendants();
+
+		const ctx = React.useMemo(() => ({ value, onChange, showLabel }), [value, onChange, showLabel]);
+
+		return (
+			<BottomNavigationProvider value={ctx}>
+				<BottomNavigationDescendantsProvider value={descendants}>
+					<StylesProvider value={styles}>
+						<chakra.nav
+							ref={ref}
+							className={cx('chakra-bottom-navigation', props.className)}
+							__css={styles.container}
+							{...ownProps}
+						/>
+					</StylesProvider>
+				</BottomNavigationDescendantsProvider>
+			</BottomNavigationProvider>
+		);
+	}
+);
+
+if (__DEV__) {
+	BottomNavigation.displayName = 'BottomNavigation';
+}
+
+BottomNavigation.defaultProps = {
+	showLabel: 'always',
+};
+
+interface IBottomNavigationItemProps extends HTMLChakraProps<'button'> {}
+
+export const BottomNavigationItem = forwardRef<IBottomNavigationItemProps, 'button'>((props, ref) => {
+	const isDisabled = props.disabled || false;
+
+	const styles = useStyles();
+
+	const context = useBottomNavigationContext();
+
+	const { index, register } = useBottomNavigationDescendant({
+		disabled: isDisabled,
+	});
+
+	const isSelected = index === context.value;
+
+	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		props.onClick?.(e);
+		context.onChange(index);
+	};
+
+	const itemStyles: SystemStyleObject = {
+		opacity: isDisabled || !isSelected ? 0.4 : 1,
+		...styles.item,
+	};
+
+	const ctx = React.useMemo(() => ({ isDisabled, isSelected }), [isDisabled, isSelected]);
+
+	return (
+		<BottomNavigationItemProvider value={ctx}>
+			<chakra.button
+				ref={mergeRefs(register, ref)}
+				className={cx('chakra-bottom-navigation__item', props.className)}
+				__css={itemStyles}
+				{...props}
+				onClick={handleClick}
+			/>
+		</BottomNavigationItemProvider>
+	);
+});
+
+if (__DEV__) {
+	BottomNavigationItem.displayName = 'BottomNavigationItem';
+}
+
+interface IBottomNavigationIconProps extends IconProps {
+	as: As<any>;
+}
+
+export const BottomNavigationIcon: React.FC<IBottomNavigationIconProps> = (props) => {
+	const styles = useStyles();
+
+	return (
+		<Icon
+			aria-hidden
+			__css={styles.icon}
+			{...props}
+			className={cx('chakra-bottom-navigation__icon', props.className)}
+		/>
+	);
+};
+
+if (__DEV__) {
+	BottomNavigationIcon.displayName = 'BottomNavigationIcon';
+}
+
+interface IBottomNavigationLabelProps extends HTMLChakraProps<'div'> {}
+
+export const BottomNavigationLabel = forwardRef<IBottomNavigationLabelProps, 'div'>((props, ref) => {
+	const { showLabel } = useBottomNavigationContext();
+	const { isSelected } = useBottomNavigationItemContext();
+	const styles = useStyles();
+
+	const dataIsLabelHidden = React.useMemo(
+		() => dataAttr(showLabel === 'never' || (showLabel === 'if-active' && !isSelected)),
+		[showLabel, isSelected]
+	);
+
+	const labelStyles: SystemStyleObject = {
+		flex: 1,
+		...styles.label,
+	};
+
+	return (
+		<StylesProvider value={styles}>
+			<chakra.div
+				ref={ref}
+				className={cx('chakra-bottom-navigation__label', props.className)}
+				__css={labelStyles}
+				data-hidden={dataIsLabelHidden}
+				{...props}
+			/>
+		</StylesProvider>
+	);
+});
+
+if (__DEV__) {
+	BottomNavigationLabel.displayName = 'BottomNavigationLabel';
+}
--- src/use-bottom-navigation.ts
+++ src/use-bottom-navigation.ts
@@ -1,1 +1,33 @@
-export const useBottomNavigation = 0;
+import { createContext } from '@chakra-ui/react-utils';
+import { createDescendantContext } from '@chakra-ui/descendant';
+
+export const [
+	BottomNavigationDescendantsProvider,
+	useBottomNavigationDescendantsContext,
+	useBottomNavigationDescendants,
+	useBottomNavigationDescendant,
+] = createDescendantContext<HTMLButtonElement>();
+
+export interface IBottomNavigationContext {
+	value: number;
+	onChange(newValue: number): void;
+	showLabel?: 'never' | 'if-active' | 'always';
+}
+
+export const [BottomNavigationProvider, useBottomNavigationContext] = createContext<IBottomNavigationContext>({
+	name: 'BottomNavigationContext',
+	errorMessage:
+		'useBottomNavigationContext: `context` is undefined. Seems you forgot to wrap the accordion components in `<BottomNavigation />`',
+});
+
+export interface IBottomNavigationItemContext {
+	isDisabled?: boolean;
+	isSelected: boolean;
+}
+
+export const [BottomNavigationItemProvider, useBottomNavigationItemContext] =
+	createContext<IBottomNavigationItemContext>({
+		name: 'BottomNavigationItemContext',
+		errorMessage:
+			'useBottomNavigationItemContext: `context` is undefined. Seems you forgot to wrap the accordion components in `<BottomNavigationItem />`',
+	});
